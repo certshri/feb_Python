e5154cdbb5197fb2e767b8aec367fc09c74d5c16
Certshri <srikant.pawar@certviewtech.com>
2019-02-09 14:09:17 +0530

    new

---
+++ Homework_New_2.py
@@ -0,0 +1,23 @@
+# for i in range(1,4):
+#     print(" ")
+#     number = 0
+#     for j in range(i, i+5):
+#
+#      print("%02d"%j,end = " ")
+#      number = j
+#     print(" ")
+#     for number in range(number,number-5,-1):
+#
+#      print("%02d"% number,end = " ")
+number = 1
+for i in range(1,4):
+    print(" ")
+
+    for j in range(1,6):
+        j=number*j
+        print("%02d"%j,end =" ")
+    print(" ")
+    for j in range(j+number,i,-i):
+        j=j-number
+        print("%02d" % j, end=" ")
+    number+=1
